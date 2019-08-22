42261f691fbbd48fcec389bc1023e2b8aa2b1c33
Tim Sawyer <Tim.Sawyer@bjss.com>
2019-08-22 16:11:14 +0100

    Test working

--- src/test/java/com/example/demo/test/DemoApplicationTests.java
+++ src/test/java/com/example/demo/test/DemoApplicationTests.java
@@ -6,8 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.demo.DemoBean;
@@ -22,13 +20,5 @@ public class DemoApplicationTests {
 	@Test
 	public void testBean() {
 		assertFalse("Should pickup test config", this.testDemoBean.getValue());
-	}
-
-	@TestConfiguration
-	public class DemoTestConfiguration {
-		@Bean
-		public DemoBean demoBean() {
-			return new TestDemoBeanImpl();
-		}
 	}
 }
--- src/test/java/com/example/demo/test/DemoTestConfiguration.java
+++ src/test/java/com/example/demo/test/DemoTestConfiguration.java
@@ -1,15 +1,16 @@
-package com.example.demo;
+package com.example.demo.test;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.demo.DemoBean;
+
 @Configuration
-@Profile("production")
-public class DemoConfiguration {
+@Profile("test")
+public class DemoTestConfiguration {
 	@Bean
 	public DemoBean demoBean() {
-		return new RealDemoBeanImpl();
+		return new TestDemoBeanImpl();
 	}
-
 }
